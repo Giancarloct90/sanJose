3dfacc193abe78240704e32cd85b607b0a2eead0
GiancarloJS <giancarloct90@hotmail.com>
2022-02-22 14:24:38 -0600

    22.02.22

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebapckPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 9000
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.html$/i,
+            loader: 'html-loader'
+        }, {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        }]
+    },
+    plugins: [
+        new HtmlWebapckPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
