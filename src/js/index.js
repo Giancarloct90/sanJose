0be27371258a6419dd73f131ae0444d0252db926
GiancarloJS <giancarloct90@hotmail.com>
2022-08-15 11:17:42 -0600

    15.08.22

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,15 @@
 // // Then we set the value in the --vh custom property to the root of the document
 // document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-// const resizeOps = () => {
-//   document.documentElement.style.setProperty(
-//     "--vh",
-//     window.innerHeight * 0.01 + "px"
-//   );
-// };
+const resizeOps = () => {
+  document.documentElement.style.setProperty(
+    "--vh",
+    window.innerHeight * 0.01 + "px"
+  );
+};
 
-// resizeOps();
-// window.addEventListener("resize", resizeOps);
+resizeOps();
+window.addEventListener("resize", resizeOps);
 
 const divBurger = document.getElementById("divBurger");
 const navLinks = document.getElementById("navLinks");
